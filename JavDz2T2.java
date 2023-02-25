5557fb9d8d809f7fe9651804e4ff0ff5cba0f5ef
SB44444 <115193888+SB44444@users.noreply.github.com>
2023-02-25 20:03:32 +0300

    Update JavDz2T2.java

--- JavDz2T2.java
+++ JavDz2T2.java
@@ -21,11 +21,11 @@ public class JavDz2T2 {
         System.out.println(content);                                                      // Вывод полученных данных в теминал 
         String [] my_array = content.split("\n");                                 // Создаем массив строк, делим по переходу строки         
         for(int i = 0; i < my_array.length; i++) {                                     // Проходим по всем эл-там массива
-            System.out.println(ShowNeed(my_array[i]));                                // Запукаем ф-цию "ПоказатьНужное" для 
+            System.out.println(ShowNeed(my_array[i]));                                // Запукаем ф-цию "ПоказатьНужное" 
         }
     }
     public static StringBuilder ShowNeed(String line) {
-        String line1 = line.replaceAll("\"", "");    // Заменяем пробелы на пустую строку
+        String line1 = line.replaceAll("\"", "");    // Заменяем перевод строки на пустую строку
         StringBuilder out_res = new StringBuilder("");             // Запускам конструктор
         String [] pers_list = line1.split(",");               // Разделяем по маске "," и получаем список с данными по каждой фамилии
         String [] need_worlds = {"Студент ", " получил ", " по предмету "};  //  Создем список слов, которые нужно добавить     
@@ -34,6 +34,6 @@ public class JavDz2T2 {
             out_res.append(need_worlds[i]);                          // Добавляем нужное слово
             out_res.append(arr[1]);                                  // Добавляем данные
             }
-        return out_res;                                             // Вщзвращаем итоговый список
+        return out_res;                                             // Возвращаем итоговый список
     }
 }
